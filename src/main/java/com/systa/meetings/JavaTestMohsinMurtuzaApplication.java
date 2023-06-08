c27b0bcaca8a8bc3ec9fc393a914953ad81cf4f8
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-06-08 18:07:57 +0100

    created spring boot project

---
+++ src/main/java/com/systa/meetings/JavaTestMohsinMurtuzaApplication.java
@@ -0,0 +1,13 @@
+package com.systa.meetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestMohsinMurtuzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestMohsinMurtuzaApplication.class, args);
+	}
+
+}
